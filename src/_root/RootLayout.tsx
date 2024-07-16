37f90e476fa9e322a90b8a8738b643dfad1d95b5
Cole Crown <colecrownwealthadrian@gmail.com>
2024-07-16 18:18:42 +0100

    update

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,6 +1,17 @@
+import LeftSidebar from "@/Components/shared/LeftSidebar"
+import TopBar from "@/Components/shared/TopBar"
+import { Outlet } from "react-router-dom"
+
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className="w-full md:flex">
+      <TopBar />
+      <LeftSidebar />
+
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+    </div>
   )
 }
 
