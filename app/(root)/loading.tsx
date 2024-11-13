b2e0c04dfeb071a7739b5e8fe2ed9de766ca70e9
Cole Crown <colecrownwealthadrian@gmail.com>
2024-11-13 09:28:47 +0100

    loading update

--- app/(root)/loading.tsx
+++ app/(root)/loading.tsx
@@ -2,29 +2,28 @@ import Image from "next/image";
 
 export default function Loading() {
   return (
-    <section className="col-flex justify-center items-center bg-black w-full min-h-screen">
-      <div className="flex justify-center items-center flex-grow">
-        <Image
-          src="/assets/images/bubble-svg.svg"
-          width={120}
-          height={120}
-          alt="Bubble logo"
-          priority
-          loading="eager"
-        />
-      </div>
-      
-      <div className="col-flex gap-4 mb-2 mt-auto">
-        <p className="text-white">from</p>
-        <Image
-          src="/assets/images/ctech.svg"
-          width={30}
-          height={30}
-          alt="C Tech Logo"
-          priority
-          loading="eager"
-        />
-      </div>
-    </section>
+    
+      <section className="flex flex-col justify-center items-center bg-black w-full min-h-screen">
+        <div className="flex justify-center items-center flex-grow">
+          <Image
+            src="/assets/images/bubble-svg.svg"
+            width={120}
+            height={120}
+            alt="Bubble logo"
+            priority
+          />
+        </div>
+        
+        <div className="flex flex-col gap-4 mb-2 mt-auto items-center">
+          <p className="text-white">from</p>
+          <Image
+            src="/assets/images/ctech.svg"
+            width={30}
+            height={30}
+            alt="C Tech Logo"
+            priority
+          />
+        </div>
+      </section>
   );
 }
